2b3e2a7f401d40970090ea99529016e267c67fa9
AzureL78 <50518907+AzureL78@users.noreply.github.com>
2021-01-07 16:26:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ echo "Please type in your password, I need to install dependencies."
 sudo apt install wget vlc git
 git clone https://github.com/AzureL78/cool-debian.git && cd cool-debian
 rm install.sh && chmod +x *.sh
-sudo ln -s $HOME/cool-debian/cool.sh /bin/cool && sudo ln -s $HOME/cool-debian/uninstall.sh /bin/uninstall
+sudo ln -s $HOME/cool-debian/cool.sh /bin/cool && sudo ln -s $HOME/cool-debian/uninstall.sh /bin/uninstall_cool
 echo "Script finished. Have fun!"
