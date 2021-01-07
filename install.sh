fd6124cfcf4219d93c14cf22215b2e9fd9b3cab7
AzureL78 <50518907+AzureL78@users.noreply.github.com>
2021-01-07 16:17:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 echo "Welcome to Cool-debian! Booting up..."
 sleep 3s
 cd $HOME
-mkdir cool && cd cool
-echo "Please type in your password, I need to install vlc and wget."
-sudo apt install wget vlc
-ls
-wget https://raw.githubusercontent.com/AzureL78/cool-debian/main/cool.sh && wget https://raw.githubusercontent.com/AzureL78/cool-debian/main/cool.mp3 && wget https://raw.githubusercontent.com/AzureL78/cool-debian/main/uninstall.sh && chmod +x $HOME/cool/cool.sh && chmod +X $HOME/cool/uninstall.sh && sudo ln -s $HOME/cool/cool.sh /bin/cool && sudo ln -s $HOME/cool/uninstall.sh /bin/uninstall_cool
-cool
+echo "Please type in your password, I need to install dependencies."
+sudo apt install wget vlc git
+git clone https://github.com/AzureL78/cool-debian.git && cd cool-debian
+rm install.sh && chmod +x *.sh
+sudo ln -s $HOME/cool-debian/cool.sh /usr/bin/cool.sh && sudo ln -s $HOME/cool-debian/uninstall.sh /usr/bin/uninstall.sh
 echo "Script finished. Have fun!"
